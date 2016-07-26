543cd5ea37b33c8c359477e58245bc4e9c0df55f
XLuoChen <1425145741@qq.com>
2016-07-26 13:34:59 +0800

    realize compareNumber when userNumbers is correct but position  is wrong

--- src/comapre-number.js
+++ src/comapre-number.js
@@ -14,13 +14,27 @@ class CompareNumber {
     const isEqual = () => {
       return this.answer === this.userNumber;
     };
-   
+
     const isWrong = () => {
-       return userNumbers.forEach(userNumber => !(answers.some(answer => answer === userNumber)));
+      return userNumbers.forEach(userNumber => !(answers.some(answer => answer === userNumber)));
     };
 
-    if (isEqual()) { a = 4; b = 0; }
-    else if (isWrong()){ a = 0; b = 0;}
+    const getCountWhenHaveWroPos = () => {
+      return userNumbers.map(userNumber => answers.some(answer => answer === userNumber) ? 1 : 0)
+        .reduce((prev, curr) => prev + curr, 0);
+    };
+
+    if (isEqual()) {
+      a = 4;
+      b = 0;
+    }
+    else if (isWrong()) {
+      a = 0;
+      b = 0;
+    }
+    else {
+      b = getCountWhenHaveWroPos();
+    }
 
     return `${a}A${b}B`;
   }
