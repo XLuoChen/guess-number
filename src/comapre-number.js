d5b40c8902064f4d2a1e23eb0c384f8e77537aff
XLuoChen <1425145741@qq.com>
2016-07-26 12:00:27 +0800

    realize compareNumber when userNumber correct completely

--- src/comapre-number.js
+++ src/comapre-number.js
@@ -1,7 +1,23 @@
+'use strict';
+
 class CompareNumber {
   constructor(answer, userNumber) {
     this.answer = answer;
     this.useNumber = userNumber;
+  }
+
+  compareNumber() {
+    let a = 0, b = 0;
+    const isEqual = () => {
+      return this.answer === this.useNumber;
+    };
+
+    if (isEqual()) {
+      a = 4;
+      b = 0;
+    }
+
+    return `${a}A${b}B`;
   }
 }
 
