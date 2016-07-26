b78dce7247bde987ef79afad57b2c5e125e57974
XLuoChen <1425145741@qq.com>
2016-07-26 15:46:46 +0800

    realize generate random answer

--- src/answer-generator.js
+++ src/answer-generator.js
@@ -1,5 +1,32 @@
-class AnswerGenerator{
+'use strict';
 
+class AnswerGenerator {
+  constructor(answer, length, isHasSame) {
+    this.answer = answer;
+    this.length = length;
+    this.isHasSame = isHasSame;
+  }
+
+  static generateAnswer() {
+    let times = 4;
+    const numbers = [];
+    let isHasSameNum;
+
+    const isHasSame = (digit) => {
+      return numbers.some(number => number === digit);
+    };
+
+    while (times) {
+      const digit = Math.floor(Math.random() * 10 + 1) % 10;
+      isHasSameNum = isHasSame(digit);
+      if (!isHasSameNum) {
+        numbers.push(digit);
+        times--;
+      }
+    }
+
+    return new AnswerGenerator(numbers.join(''), numbers.length, isHasSameNum);
+  }
 }
 
 module.exports = AnswerGenerator;
