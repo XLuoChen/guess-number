228a7dd0d30906ad0af0e24767107cc4aa6ea998
XLuoChen <1425145741@qq.com>
2016-07-27 00:31:55 +0800

    refactor test file of compare-number-spec.js

--- spec/compare-num-spec.js
+++ spec/compare-num-spec.js
@@ -2,31 +2,25 @@
 
 const CompareNumber = require('../src/comapre-number');
 
-describe('compare number', () => {
+describe('Compare Number', () => {
 
-  it('should return string 4A0B when userNumber correct completely', () => {
-
-    const string = new CompareNumber().compareNumber('1234', '1234');
-
-    expect(string).toEqual('4A0B');
-  });
-
-  it('should return string 0A0B when userNumber is wrong completely', () => {
-    const string = new CompareNumber().compareNumber('1234', '5678');
-
-    expect(string).toEqual('0A0B');
-  });
-
-  it("should return string 0A4B when userNumbers is correct but positions is wrong", () => {
-    const string = new CompareNumber().compareNumber('1234', '4321');
-
-    expect(string).toEqual('0A4B');
-  });
-
-  it('should return string 2A2B when both have correct numbers and correct', () => {
-    const string = new CompareNumber().compareNumber('1234', '1243');
-
-    expect(string).toEqual('2A2B');
-  });
+  it('returns xAxB', () => {
+    [{
+      input: '1234',
+      answer: '5678',
+      result: '0A0B'
+    },{
+      input: '1234',
+      answer: '1234',
+      result: '4A0B'
+    },{
+      input: '1234',
+      answer: '1243',
+      result: '2A2B'
+    }].forEach(example => {
+      const result = new CompareNumber().compareNumber(example.answer,example.input);
+      expect(result).toEqual(example.result);
+    })
+  })
 });
 
