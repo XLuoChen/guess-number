56ab114a69e13b8555299ecc23b0f7dd29d20cf8
XLuoChen <1425145741@qq.com>
2016-07-26 11:25:42 +0800

    create class CompareNumber

---
+++ spec/compare-num-spec.js
@@ -0,0 +1,3 @@
+/**
+ * Created by never on 16-7-26.
+ */
---
+++ src/comapre-number.js
@@ -0,0 +1,8 @@
+class CompareNumber {
+  constructor(answer, userNumber) {
+    this.answer = answer;
+    this.useNumber = userNumber;
+  }
+}
+
+module.exports = CompareNumber;
