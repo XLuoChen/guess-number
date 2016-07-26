6b7ea1696af899679256c823fff1f4b4eae88ed5
XLuoChen <1425145741@qq.com>
2016-07-26 17:59:08 +0800

    refactor code

--- spec/answer-generator-spec.js
+++ spec/answer-generator-spec.js
@@ -3,15 +3,16 @@
 const AnswerGenerator = require('../src/answer-generator');
 
 describe('generate answer', () => {
-  it('should generate answer whose length is 4', () => {
-    const answer = AnswerGenerator.generateAnswer();
+  it('should generate answer', () => {
 
+    const isUnique = (item, index, array) => {
+      return array.lastIndexOf(item) === index;
+    };
+
+    const answer = AnswerGenerator.generateAnswer();
     expect(answer.length).toEqual(4);
-  });
-
-  it('should generate answer which has not same numbers', () => {
-    const answer = AnswerGenerator.generateAnswer();
-
-    expect(answer.isHasSame).toEqual(false);
+    expect(answer.every(isUnique)).toBeTruthy();
+    expect(AnswerGenerator.generateAnswer()).not.toEqual(AnswerGenerator.generateAnswer());
   });
 });
+
