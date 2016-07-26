eaca6070a34ace9b88c02810c9f763f0e98a99c0
XLuoChen <1425145741@qq.com>
2016-07-26 12:04:49 +0800

    write test file when userNumber is wrong completely

--- spec/compare-num-spec.js
+++ spec/compare-num-spec.js
@@ -1,11 +1,19 @@
+'use strict';
+
 const CompareNumber = require('../src/comapre-number');
 
 describe('compare number', () => {
 
   it('should return string 4A0B when userNumber correct completely', () => {
 
-    const string = new CompareNumber('1234','1234').compareNumber();
+    const string = new CompareNumber('1234', '1234').compareNumber();
 
     expect(string).toEqual('4A0B');
-  })
+  });
+
+  it('should return string 0A4B when userNumber is wrong completely', () => {
+    const string = new CompareNumber('1234', '5678').compareNumber();
+
+    expect(string).toEqual('0A0B');
+  });
 });
