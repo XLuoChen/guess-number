59479435a51d47d5357530476e93518fa7a7865b
XLuoChen <1425145741@qq.com>
2016-07-26 13:17:10 +0800

    realize compareNumber when userNumber wrong completely

--- src/comapre-number.js
+++ src/comapre-number.js
@@ -3,19 +3,24 @@
 class CompareNumber {
   constructor(answer, userNumber) {
     this.answer = answer;
-    this.useNumber = userNumber;
+    this.userNumber = userNumber;
   }
 
   compareNumber() {
     let a = 0, b = 0;
+    const answers = this.answer.split('');
+    const userNumbers = this.userNumber.split('');
+
     const isEqual = () => {
-      return this.answer === this.useNumber;
+      return this.answer === this.userNumber;
+    };
+   
+    const isWrong = () => {
+       return userNumbers.forEach(userNumber => !(answers.some(answer => answer === userNumber)));
     };
 
-    if (isEqual()) {
-      a = 4;
-      b = 0;
-    }
+    if (isEqual()) { a = 4; b = 0; }
+    else if (isWrong()){ a = 0; b = 0;}
 
     return `${a}A${b}B`;
   }
