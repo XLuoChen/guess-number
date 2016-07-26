b93341fa6f1e67b1258b49b93ee7e796468cd616
XLuoChen <1425145741@qq.com>
2016-07-26 14:00:26 +0800

    refactor code

--- src/comapre-number.js
+++ src/comapre-number.js
@@ -7,42 +7,24 @@ class CompareNumber {
   }
 
   compareNumber() {
-    let a = 0, b = 0;
+
     const answers = this.answer.split('');
     const userNumbers = this.userNumber.split('');
 
-    const isEqual = () => {
-      return this.answer === this.userNumber;
-    };
-
-    const isWrong = () => {
-      return userNumbers.forEach(userNumber => !(answers.some(answer => answer === userNumber)));
-    };
-
-    const getCountWrongPos = () => {
+    const getWrongPosCount = () => {
       return userNumbers.map((userNumber, index) =>
           answers[index] != userNumber && answers.some(answer => answer === userNumber)
             ? 1 : 0)
         .reduce((prev, curr) => prev + curr, 0);
     };
 
-    const getCountCorrectNumber = () => {
+    const getCorrectNumberCount = () => {
       return userNumbers.map((userNumber, index) => answers[index] === userNumber ? 1 : 0)
         .reduce((prev, curr) => prev + curr, 0);
     };
 
-    if (isEqual()) {
-      a = 4;
-      b = 0;
-    }
-    else if (isWrong()) {
-      a = 0;
-      b = 0;
-    }
-    else {
-      a = getCountCorrectNumber();
-      b = getCountWrongPos();
-    }
+    const a = getCorrectNumberCount();
+    const b = getWrongPosCount();
 
     return `${a}A${b}B`;
   }
