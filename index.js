f060e0cd5a4f0296e66f15155dd879c5f795ef00
XLuoChen <1425145741@qq.com>
2016-07-27 00:21:02 +0800

    create startup file

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const Game = require('./src/game');
+
+Game.game();
--- spec/game-spec.js
+++ spec/game-spec.js
@@ -3,12 +3,11 @@
 const Game = require('../src/game');
 const AnswerGenerator = require('../src/answer-generator');
 
-describe('Game start',() => {
-  xit("should print 'congratulations' when input correctly during times",() => {
-    spyOn(AnswerGenerator,'generateAnswer').and.returnValue('1234');
+describe('Game start', () => {
+  it("should print 'congratulations' when input correctly during times", () => {
+    spyOn(AnswerGenerator, 'generateAnswer').and.returnValue('1234');
     Game.game();
-    spyOn(console,'log');
+    spyOn(console, 'log');
     expect(console.log).toHaveBeenCalledWith('Congratulations!');
   });
-
 });
