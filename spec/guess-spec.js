1e7ba617be004042e3294bbd5154fa90f351c121
XLuoChen <1425145741@qq.com>
2016-07-26 18:07:29 +0800

    write test file for Guess

---
+++ spec/guess-spec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const AnswerGenerator = require('../src/answer-generator');
+const Guess = require('../src/guess');
+
+describe('Guess',() => {
+  it('should play the game', () => {
+    spyOn(AnswerGenerator,'generateAnswer').and.returnValue('1234');
+    const result = Guess.guess('1234');
+    expect(result).toEqual('4A0B');
+  })
+});
