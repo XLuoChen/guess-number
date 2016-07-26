779f3a6dca9c4577b994f5fa0550915a3ab65541
XLuoChen <1425145741@qq.com>
2016-07-26 21:09:45 +0800

    write test file for game

--- spec/game-spec.js
+++ spec/game-spec.js
@@ -1,6 +1,20 @@
+'use strict';
+
+const Game = require('../src/game');
+const AnswerGenerator = require('../src/answer-generator');
+
 describe('Game start',() => {
   it("should print 'congratulations' when input correctly during times",() => {
+    spyOn(AnswerGenerator,'generateAnswer').and.returnValue('1234');
+    Game.game();
     spyOn(console,'log');
-    expect(console.log()).toHaveBeenCalledWith('congratulations');
+    expect(console.log).toHaveBeenCalledWith('Congratulations!');
+  });
+
+  it("should print 'Game Over!' when input wrong and beyond times",() => {
+    spyOn(AnswerGenerator,'generateAnswer').and.returnValue('1234');
+    Game.game();
+    spyOn(console,'log');
+    expect(console.log).toHaveBeenCalledWith('Game Over!');
   });
 });
