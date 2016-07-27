44a2c51843bce8f3ad486afca4df3bb2dc036db0
XLuoChen <1425145741@qq.com>
2016-07-27 11:23:42 +0800

    modified game spec and src

--- spec/game-spec.js
+++ spec/game-spec.js
@@ -2,12 +2,15 @@
 
 const Game = require('../src/game');
 const AnswerGenerator = require('../src/answer-generator');
+const stdin = require('mock-stdin').stdin();
 
 describe('Game start', () => {
   it("should print 'congratulations' when input correctly during times", () => {
     spyOn(AnswerGenerator, 'generateAnswer').and.returnValue('1234');
-    Game.game();
     spyOn(console, 'log');
+    Game.game();
+    stdin.send('1234');
+    
     expect(console.log).toHaveBeenCalledWith('Congratulations!');
   });
 });
--- src/game.js
+++ src/game.js
@@ -2,36 +2,39 @@
 
 const AnswerGenerator = require('./answer-generator');
 const CompareNumber = require('./comapre-number');
-const scanf = require('scanf');
 
 class Game {
 
   static game() {
+    const answer = AnswerGenerator.generateAnswer();
+
     const isDuplicate = (item, index, array) => {
       return array.lastIndexOf(item) != index;
     };
-
-    const answer = AnswerGenerator.generateAnswer();
-    console.log('Welcome!');
-    for (let i = 0; i < 6; i++) {
-      console.log('Please input your number(6):');
-      const userNumber = scanf('%s');
-      if (userNumber.split('').every(isDuplicate)) {
-        console.log('Cannot input duplicate numbers!');
-      }
-      else {
-        const text = CompareNumber.compareNumber(answer, userNumber);
-        if (text === '4A0B') {
-          console.log('Congratulations!');
-          return;
+    process.stdin.setEncoding('utf8');
+    process.stdin.on('data', (userNumber) => {
+      console.log('Welcome!');
+      for (let i = 0; i < 6; i++) {
+        console.log('Please input your number(6):');
+        if (userNumber.split('').every(isDuplicate)) {
+          console.log('Cannot input duplicate numbers!');
         }
         else {
-          console.log(text);
+          const text = CompareNumber.compareNumber(answer, userNumber);
+          if (text === '4A0B') {
+            console.log('Congratulations!');
+            return;
+          }
+          else {
+            console.log(text);
+          }
         }
       }
-    }
-    console.log('Game Over!');
+      console.log('Game Over!');
+    });
   }
 }
+
+Game.game();
 
 module.exports = Game;
